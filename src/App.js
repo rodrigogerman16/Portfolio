a8f7336f3bb60410388d06a9cfa299ed1ade35cc
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-06-18 10:42:46 +0200

    First comit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,93 @@
 import './App.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faRotateBack, faPlay } from '@fortawesome/free-solid-svg-icons';
 import Form from './Components/Form.js';
+import React, { useState } from 'react';
 
 function App() {  
+
+  const [toggle, setToggle] = useState(true);
+
+  const switchHeader = () => {
+    setToggle(!toggle);
+  }
+
   return (
     <div className="App">
       
       {/* Navbar */}
-      <navbar>
+      <div>
         <ul className='Navbar'>
           <a href="#header">About me</a>
           <a href="#work">Work</a>
           <a href="#footer">Contact</a>        
         </ul>
-      </navbar>
+      </div>
 
       {/* Header */}
       <header className='Header' id="header">
-        <div className='Header-title'>
-          <div><h2>Hi,</h2></div>
-          <div className='Name'><h2>I'm Rodrigo,</h2></div>
-          <div className='Description'><p>Front End Developer</p></div> 
+
+        <div className='Header-initial'>
+          <div className='Header-title'>
+            <div><h2>Hi,</h2></div>
+            <div className='Name'><h2>I'm Rodrigo,</h2></div>
+            <div className='Description'><p>Front End Developer</p></div> 
+          </div>
+          <div className='Headerbtn'>
+            <button onClick={switchHeader}><FontAwesomeIcon icon={faPlay}/></button>
+          </div>
         </div>
+
+        <div className='Header-skills'>
+          <div className='Header-title'>
+            <div><h2>I like,</h2></div>
+            <div className='Name'><h2>To create,</h2></div>
+            <div className='Description'><p>Amazing experiences</p></div> 
+          </div>
+          <div className='Headerbtn'>
+            <button><FontAwesomeIcon icon={faRotateBack} /></button>
+          </div>
+        </div>
+
       </header>
 
       {/* Body */}
-      <body className='Body' id="work">
+      <div className='Body' id="work">
         <div className='Proyects'>
-          <span></span>
-          <span></span>
+          <span>
+            <div className='Proyects-img'>
+              <img src='mypiroska.png' />
+            </div>
+            <h4>My Piroska</h4>
+            <p>Lore ipsum</p>
+          </span>
+          <span>
+            <div className='Proyects-img'>
+              <img src='belenpage.png' />
+            </div>
+            <h4>Belen Tejedor Page</h4>
+            <p>Lore ipsum</p>
+          </span>
+          <span>
+            <div className='Proyects-img'>
+              <img src='mypiroska.png' />
+            </div>
+            <h4>Geo Quizz</h4>
+            <p>Lore ipsum</p>
+          </span>
+          <span>
+            <div className='Proyects-img'>
+              <img src='mypiroska.png' />
+            </div>
+            <h4>Weather App</h4>
+            <p>Lore ipsum</p>
+          </span>
         </div>
-      </body>
+      </div>
 
       {/* Footer */}
-      <footer className='Footer' id="footer">
+      <div className='Footer' id="footer">
         <Form />
         <div className='Social-icons'>
         <a href="https://www.instagram.com/rodrigogerman2/?hl=es"><FontAwesomeIcon icon={faInstagram} /></a>
@@ -42,7 +95,7 @@ function App() {
         <a href="https://github.com/rodrigogerman16"><FontAwesomeIcon icon={faGithub} /></a>
         <a href="https://www.linkedin.com/in/rodrigo-german-8408a5127/"><FontAwesomeIcon icon={faLinkedin} /></a>
         </div>
-      </footer>
+      </div>
 
     </div>
   );
