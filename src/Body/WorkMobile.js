9156839e1570197970ebe098844e5164af3acd8a
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-06-26 22:22:12 +0200

    Work slider js

---
+++ src/Body/WorkMobile.js
@@ -0,0 +1,27 @@
+import classes from "./Work.module.css"
+import Work from './WorkImages'
+import React from 'react'  
+import { motion } from 'framer-motion'
+
+const WorkMobile = () =>{
+    return(
+    <motion.div className={classes.WorkSliderContainer}>
+      <motion.div  drag='x' dragConstraints={{right: 0, left: -550}}>
+        {Work.map(Work => (
+          <motion.div className={classes.WorkSlider }>
+            <motion.div className={classes.WorkImg }>
+              <img src={Work} alt=''/>
+            </motion.div>            
+            <motion.div className={classes.WorkDescription}>
+              <h3>My Piroska</h3>
+              <p>Lore ipsum</p>
+            </motion.div>
+          </motion.div>
+          
+        ))}
+      </motion.div>
+    </motion.div>
+    )
+}
+
+export default WorkMobile
