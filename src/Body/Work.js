75b9d4be300e29c2d23bc5ba2515e83d1c783417
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-06-26 22:22:33 +0200

    Work.js

---
+++ src/Body/Work.js
@@ -0,0 +1,13 @@
+import WorkDesktop from "./WorkDesktop"
+import WorkMobile from "./WorkMobile"
+
+const Work = () =>{
+    return(
+        <div>
+            <WorkDesktop/>
+            <WorkMobile/>
+        </div>
+    )
+}
+
+export default Work
