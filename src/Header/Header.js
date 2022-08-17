8d1a07e9bb694c1bb21ef1d9ecd039ce88e1e34c
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-08-17 18:35:05 +0200

    Bubbles eliminated Header combinated

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,51 +1,15 @@
 import classes from "./Header.module.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faRotateBack } from '@fortawesome/free-solid-svg-icons';
-import { useState } from "react";
 
 const Header = () => {
-
-  const [isActive, setIsActive] = useState(true);
-
-  const handleClick = event => {
-    setIsActive(current => !current);
-    console.log(handleClick)
-  };
-
-    return(
-      
+    return(      
         <div className={classes.HeaderInitial}>
-          {isActive && <div className={classes.HeaderTitle}>
-            <div><h2>Hi</h2></div>
-            <div><h2>I'm Rodrigo</h2></div>
-            <div><p>Front End Developer</p></div> 
-          </div>
-          }
-
-          {!isActive && <div className={classes.HeaderTitle}>
-            <div><h2>I like</h2></div>
-            <div><h2>To create</h2></div>
-            <div><p>Amazing experiences</p></div> 
-          </div>}
-          
-          <div className={classes.HeaderBtn} onClick={handleClick}>
-                {isActive && <button><FontAwesomeIcon icon={faPlay}/></button>}
-                
-                {!isActive && 
-                <div className={classes.BubbleContainer}>    
-                 <button><FontAwesomeIcon icon={faRotateBack}/></button>             
-                  <div className={classes.Bubble}><p>Jquery</p></div>
-                  <div className={classes.Bubble}><p>React</p></div>
-                  <div className={classes.Bubble}><p>JavaScript</p></div>
-                  <div className={classes.Bubble}><p>Css</p></div>
-                  <div className={classes.Bubble}><p>Html</p></div>
-                  <div className={classes.Bubble}><p>Git</p></div>
-                  <div className={classes.Bubble}><p>Github</p></div>
-                  <div className={classes.Bubble}><p>Vercel</p></div>
-                </div>}
-            </div>
-        </div>
-      
+          <div className={classes.HeaderTitle}>
+            <h2>Hi</h2>
+            <h2>I'm Rodrigo</h2>
+            <p>Front End Developer</p>
+            <h6>I like to create amazing experiences</h6>
+          </div>            
+        </div>      
     )
 }
 
