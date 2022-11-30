8c68ffe6318a4f44bfd6120bfeb6862302bff52b
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-11-30 22:06:58 +0100

    Added title in work component

--- src/Body/Work.js
+++ src/Body/Work.js
@@ -1,9 +1,12 @@
 import WorkDesktop from "./WorkDesktop"
 import WorkMobile from "./WorkMobile"
+import classes from "./Work.module.css"
 
 const Work = () =>{
     return(
         <div>
+            
+            <h2 className={classes.WorkTitle} id='work'>WORK</h2>
             <WorkDesktop/>
             <WorkMobile/>
         </div>
