f9e612f864d19f3cb4bf71d9c122b10405eef794
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-14 15:52:26 +0100

    Work Component Cleaned

--- src/Body/Work.js
+++ src/Body/Work.js
@@ -1,14 +1,9 @@
 import WorkDesktop from "./WorkDesktop"
-import WorkMobile from "./WorkMobile"
-import classes from "./Work.module.css"
 
 const Work = () =>{
     return(
-        <div>
-            
-            <h2 className={classes.WorkTitle} id='work'>WORK</h2>
+        <div>            
             <WorkDesktop/>
-            <WorkMobile/>
         </div>
     )
 }
