7251076bba1609e953ea3f1744f1c756fb323425
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-14 15:52:06 +0100

    Deleted workMobile components

--- src/Body/WorkImages.js
+++
@@ -1,6 +0,0 @@
-import work1 from "../Assets/belenpage.png"
-import work2 from "../Assets/quizapp.png"
-import work3 from "../Assets/myweatherapp.png"
-import work4 from "../Assets/timerapp.png"
-
-export default [work1, work2, work3, work4]
