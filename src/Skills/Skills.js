3dedd7803d3ae06687cac65c0794bb34df53b6ef
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-15 05:13:20 +0100

    Skills Container created

---
+++ src/Skills/Skills.js
@@ -0,0 +1,39 @@
+import classes from './Skills.module.css'
+
+const Skills = () =>{
+    return(
+        <div className={classes.SkillsContainer}>
+            <div className={classes.SkillsTitle}>
+                <h2>SKILLS</h2>
+            </div>
+            <div className={classes.SkillsItemsContainer}>
+                <div className={classes.SkillItem}>
+                    <img src='/leader.png' alt='Leaderpng'/>
+                    <p>Leader</p>
+                </div>
+                <div className={classes.SkillItem}>
+                    <img src='/energetic.png' alt='energeticpng'/>
+                    <p>Energetic</p>
+                </div>
+                <div className={classes.SkillItem}>
+                    <img src='/optimistic.png' alt='optimisticpng'/>
+                    <p>Optimistic</p>
+                </div>
+                <div className={classes.SkillItem}>
+                    <img src='/orator.png' alt='oratorpng'/>
+                    <p>Orator</p>
+                </div>
+                <div className={classes.SkillItem}>
+                    <img src='/friendly.png' alt='friendlypng'/>
+                    <p>Friendly</p>
+                </div>
+                <div className={classes.SkillItem}>
+                    <img src='/autodidact.png' alt='autodidactpng'/>
+                    <p>Autodidact</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
