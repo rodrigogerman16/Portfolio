24df64bbd59cc59ef03b1b5af19768ed096b9aa4
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-15 23:14:43 +0100

    feat: stack animated

--- src/Stack/Stack.js
+++ src/Stack/Stack.js
@@ -1,54 +1,60 @@
 import classes from "./Stack.module.css"
+import { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Stack = () =>{
+    useEffect(() =>{
+        AOS.init({duration: 1000})
+      }, [])
     return(
         <div className={classes.StackContainer}>
-            <div className={classes.StackTitle}>
+            <div data-aos='fade-left' className={classes.StackTitle}>
                 <h2>STACK</h2>
             </div>
             <div className={classes.StackImgContainer}>
 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/html.png' alt='htmlpng' />
                     <p>HTML5</p>
                 </div>
 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/css.png' alt='csspng'/>
                     <p>CSS</p>
                 </div>
 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/javascript.png' alt='javascriptpng'/>
                     <p>JavaScript</p>
                 </div>
 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/git.png' alt='gitpng'/>
                     <p>Git</p>
                 </div>
 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/react.png' alt='reactpng'/>
                     <p>React</p>
                 </div>
                 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/redux.png' alt='reduxpng'/>
                     <p>Redux</p>
                 </div>
                 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/nodejs.png' alt='nodejspng'/>
                     <p>NodeJs</p>
                 </div>
                 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/postgresql.png' alt='postgresqlpng'/>
                     <p>PostgreSql</p>
                 </div>
 
-                <div>
+                <div data-aos="zoom-in-up">
                     <img src='/sequelize.png' alt='sequelizepng'/>
                     <p>Sequelize</p>
                 </div>
