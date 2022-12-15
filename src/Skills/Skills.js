2c7833e749c8a1568b9e9394f1c1ade231f86441
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-15 02:04:33 +0100

    changed png, added text and modify css

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -4,16 +4,55 @@ const Skills = () =>{
     return(
         <div className={classes.SkillsContainer}>
             <div className={classes.SkillsTitle}>
-                <h2>SKILLS</h2>
+                <h2>STACK</h2>
             </div>
             <div className={classes.SkillsImgContainer}>
-                <img src='/html.png' alt='htmlpng' />
-                <img src='/css.png' alt='csspng'/>
-                <img src='/javascript.png' alt='javascriptpng'/>
-                <img src='/react.png' alt='reactpng'/>
-                <img src='/redux.png' alt='reduxpng'/>
-                <img src='/nodejs.png' alt='nodejspng'/>
-                <img src='/sequelize.png' alt='sequelizepng'/>
+
+                <div>
+                    <img src='/html.png' alt='htmlpng' />
+                    <p>HTML</p>
+                </div>
+
+                <div>
+                    <img src='/css.png' alt='csspng'/>
+                    <p>CSS</p>
+                </div>
+
+                <div>
+                    <img src='/javascript.png' alt='javascriptpng'/>
+                    <p>JavaScript</p>
+                </div>
+
+                <div>
+                    <img src='/git.png' alt='gitpng'/>
+                    <p>Git</p>
+                </div>
+
+                <div>
+                    <img src='/react.png' alt='reactpng'/>
+                    <p>React</p>
+                </div>
+                
+                <div>
+                    <img src='/redux.png' alt='reduxpng'/>
+                    <p>Redux</p>
+                </div>
+                
+                <div>
+                    <img src='/nodejs.png' alt='nodejspng'/>
+                    <p>NodeJs</p>
+                </div>
+                
+                <div>
+                    <img src='/postgresql.png' alt='postgresqlpng'/>
+                    <p>PostgreSql</p>
+                </div>
+
+                <div>
+                    <img src='/sequelize.png' alt='sequelizepng'/>
+                    <p>Sequelize</p>
+                </div>
+                               
             </div>
         </div>
    
