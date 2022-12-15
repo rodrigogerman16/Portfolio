d5e7350798ea34df628273e9d53a723264683aef
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-15 23:08:55 +0100

    feat: changed animations in work

--- src/Body/WorkDesktop.js
+++ src/Body/WorkDesktop.js
@@ -6,20 +6,14 @@ import 'aos/dist/aos.css';
 const WorkDesktop = () =>
 {
   useEffect(() =>{
-    AOS.init({duration: 11500})
+    AOS.init({duration: 1000})
   }, [])
     return(
         <div className={classes.Body}>
-        <h2 data-aos='fade-down'
-             data-aos-offset="300"
-             data-aos-easing="ease-in-sine"
-             data-aos-duration="500" className={classes.WorkTitle}>Some of my latest work</h2>
+        <h2 data-aos='fade-right' className={classes.WorkTitle}>Some of my latest work</h2>
         <div className={classes.Work}>
           
-          <a data-aos='fade-right'
-             data-aos-offset="300"
-             data-aos-easing="ease-in-sine"
-             data-aos-duration="500" className={classes.WorkCard} href='https://belentejedor.vercel.app/' target="_blank" rel="noreferrer">
+          <a data-aos='zoom-in' className={classes.WorkCard} href='https://belentejedor.vercel.app/' target="_blank" rel="noreferrer">
             <div className={classes.WorkImg}>
               <img src='belenpage.png' alt='belenpage'/>
             </div>
@@ -28,10 +22,7 @@ const WorkDesktop = () =>
             </div>
           </a>          
 
-          <a data-aos='fade-right'
-             data-aos-offset="300"
-             data-aos-easing="ease-in-sine"
-             data-aos-duration="500" className={classes.WorkCard} href='https://weather-app-mocha-alpha.vercel.app/' target="_blank" rel="noreferrer"> 
+          <a data-aos='zoom-in' className={classes.WorkCard} href='https://weather-app-mocha-alpha.vercel.app/' target="_blank" rel="noreferrer"> 
             <div className={classes.WorkImg}>
               <img src='myweatherapp.png' alt='weatherapp-img'/>
             </div>
@@ -40,10 +31,7 @@ const WorkDesktop = () =>
             </div>
           </a>
 
-          <a data-aos='fade-right'
-             data-aos-offset="300"
-             data-aos-easing="ease-in-sine"
-             data-aos-duration="500" className={classes.WorkCard} href='https://timer-app-eight.vercel.app/' target="_blank" rel="noreferrer">
+          <a data-aos='zoom-in' className={classes.WorkCard} href='https://timer-app-eight.vercel.app/' target="_blank" rel="noreferrer">
             <div className={classes.WorkImg}>
               <img src="timerapp.png" alt="timerapp-img"/>
             </div>
@@ -52,10 +40,7 @@ const WorkDesktop = () =>
             </div>
           </a>          
 
-          <div data-aos='fade-right'
-             data-aos-offset="300"
-             data-aos-easing="ease-in-sine"
-             data-aos-duration="500" className={classes.WorkCard} href='https://geoquiz-mpgsgymel-rodrigogerman16.vercel.app/' target="_blank" rel="noreferrer">
+          <div data-aos='zoom-in' className={classes.WorkCard} href='https://geoquiz-mpgsgymel-rodrigogerman16.vercel.app/' target="_blank" rel="noreferrer">
             <div className={classes.WorkImg}>
               <img src='quizapp.png' alt='geoquizz'/>
             </div>
