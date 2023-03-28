dd0f74ca0fb26c4b0dd08014b9002d8232ff295d
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-03-29 01:36:13 +0200

    global css color changed from red to blue

--- src/Footer/Form.js
+++ src/Footer/Form.js
@@ -1,47 +1,77 @@
-import React, {Fragment, useState}  from 'react';
-import emailjs from 'emailjs-com';
+import React, { Fragment, useState } from "react";
+import emailjs from "emailjs-com";
 
 const Form = () => {
+  const [datos, setDatos] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
 
-    const [datos, setDatos] = useState ({
-        name: '',
-        phone: '',
-        email: '',
-        message: '',
-    })
+  const handleInputChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleInputChange = (e) => {
-        setDatos({
-            ...datos,
-        [e.target.name] : e.target.value
-        })        
-    }
+  const sendData = (e) => {
+    e.preventDefault();
 
-    const sendData = (e) => {
-        e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_ki8skwr",
+        "template_g1hml5e",
+        e.target,
+        "3JrmPJmxGZpTsDh1a"
+      )
+      .then((res) => {
+        alert("Your message has been sent.");
+      });
+  };
 
-        emailjs.sendForm('service_ki8skwr', 'template_g1hml5e', e.target, '3JrmPJmxGZpTsDh1a').then (res=>{
-            alert('Your message has been sent.');
-    })
-    }
-
-    return (
-        <Fragment>
-            <div className='Contact-container' id='footer' onSubmit={sendData}>
-            <div className='Message-form'>
-            <h1>¡Let's work together!</h1>
-            <div className='Border'></div>  
-            <form className='Contact-form' action="index.html" method="post">
-                <input type="text" className='Contact-form-text' placeholder='Name' name='name' onChange={handleInputChange}/>
-                <input type="text" className='Contact-form-text' placeholder='Phone' name='phone'onChange={handleInputChange}/>
-                <input type="email" className='Contact-form-text' placeholder='Email' name='email'onChange={handleInputChange}/>
-                <textarea type="text" className='Contact-form-text' placeholder='Message' name='message'onChange={handleInputChange}/>
-                <input type="submit" className='Contact-form-btn' value='Send'/>
-            </form>
-            </div>      
+  return (
+    <Fragment>
+      <div className="Contact-container" id="footer" onSubmit={sendData}>
+        <div className="Message-form">
+          <h1>¡Let's work together!</h1>
+          <div className="Border"></div>
+          <form className="Contact-form" action="index.html" method="post">
+            <input
+              type="text"
+              className="Contact-form-text"
+              placeholder="Name"
+              name="name"
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              className="Contact-form-text"
+              placeholder="Phone"
+              name="phone"
+              onChange={handleInputChange}
+            />
+            <input
+              type="email"
+              className="Contact-form-text"
+              placeholder="Email"
+              name="email"
+              onChange={handleInputChange}
+            />
+            <textarea
+              type="text"
+              className="Contact-form-text"
+              placeholder="Message"
+              name="message"
+              onChange={handleInputChange}
+            />
+            <input type="submit" className="Contact-form-btn" value="Send" />
+          </form>
         </div>
-      </Fragment>
-    );
-}
+      </div>
+    </Fragment>
+  );
+};
 
 export default Form;
