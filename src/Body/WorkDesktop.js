398742d61e25fae7c39288e370bb8dcf58edb069
Rodrigo <rodrigoezequielgerman@gmail.com>
2025-07-15 21:35:54 +0200

    Feat: new project Sleeve It

--- src/Body/WorkDesktop.js
+++ src/Body/WorkDesktop.js
@@ -10,7 +10,7 @@ const WorkDesktop = () => {
   return (
     <div className={classes.Body} id="work">
       <div className={classes.Work}>
-        {/*Woffinder*/}
+        {/*Mercado Solidario*/}
         <div data-aos="zoom-in" className={classes.WorkCard}>
           <div className={classes.WorkImg}>
             <img
@@ -37,6 +37,49 @@ const WorkDesktop = () => {
             </p>
             <a
               href="https://mercadosolidario.vercel.app/"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              target="_blank"
+              rel="noreferrer"
+            >
+              See more
+              <svg
+                className="-mr-1 ml-2 h-4 w-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+ {/*Sleeve It*/}
+        <div data-aos="zoom-in" className={classes.WorkCard}>
+          <div className={classes.WorkImg}>
+            <img
+              src="sleeveit.jpg" alt="Sleeve It Logo"
+            />
+          </div>
+          <div className="p-5">
+            <h5 className="text-white font-bold text-2xl tracking-tight mb-2">
+              Sleeve It
+            </h5>
+            <div className="flex flex-wrap gap-x-1 gap-y-2 pb-3 md:pb-10">
+              <span className="bg-blue-500 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Next.js</span>
+              <span className="bg-yellow-400 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded">TypeScript </span>
+              <span className="bg-white text-black text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Tailwind</span>
+              <span className="bg-cyan-500 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Supabase </span>
+            </div>
+            <p className="font-normal text-gray-400 mb-3 md:h-20">
+              Find the perfect sleeves for any board game, just search the title and get the sleeve sizes you need
+            </p>
+            <a
+              href="https://v0-sleeve-it-app.vercel.app/"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               target="_blank"
               rel="noreferrer"
